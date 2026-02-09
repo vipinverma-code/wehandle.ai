9192883cbe7b644853ac1bcee34c3b626dd49943
vipinverma <vipinverma4224@gmail.com>
2026-02-10 01:03:51 +0530

    chnages done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Navbar from './components/Navbar'
 import { useState } from 'react';
 const App = () => {
-  const[theme, setTheme] = useState('light')
+  const[theme, setTheme] = useState('dark')
   return (
     <div className='dark:bg-black relative'>
       <Navbar theme={theme} setTheme={setTheme}/>
