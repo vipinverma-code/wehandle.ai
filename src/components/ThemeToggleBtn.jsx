6dcbd7ca1b02945235459bcf57c92a26ba5f2734
vipinverma <vipinverma4224@gmail.com>
2026-02-12 02:35:04 +0530

    changes done

---
+++ src/components/ThemeToggleBtn.jsx
@@ -0,0 +1,20 @@
+import assets from "../assets/assets"
+const ThemeToggleBtn = ({theme, setTheme}) => {
+  return (
+    <>
+      <button>
+        {theme === 'dark' ? (
+            <img onClick= {()=>{
+                setTheme('light')}} src={assets.sun_icon} className='size-8.5 p-1.5 border-gray-500 rounded-full' alt="sun-icon" />
+            ) 
+        : (
+            <img onClick={()=>{
+                 setTheme('dark')
+            }} src={assets.moon_icon} className='size-8.5 p-1.5 border border-gray-500 rounded-full'alt="" />
+        )}
+      </button>
+    </>
+  )
+}
+
+export default ThemeToggleBtn
