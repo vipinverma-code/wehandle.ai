ef0b99c2be1fdbfab0b8dd7d5af3c8349960e326
vipinverma <vipinverma4224@gmail.com>
2026-09-25 13:56:35 +0530

    changes done

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,11 @@ import { useState } from 'react';
 const App = () => {
   const[theme, setTheme] = useState(localStorage.getItem('theme') ? localStorage.getItem('theme'): 'light')
   return (
-    <div className="bg-[--color-primary] text-white">
+    <div className="min-h-screen bg-white text-gray-700 dark:bg-gray-950 dark:text-white transition-colors duration-300">
       <Navbar theme={theme} setTheme={setTheme} />
       <Hero />
-      <TrustedBy/>
-      <Services/>
+      <TrustedBy />
+      <Services />
     </div>
   );
 };
