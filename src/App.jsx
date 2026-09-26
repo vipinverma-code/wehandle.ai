a67805d1148ead9cc36851c63e4d6a0ddd1e2a79
vipinverma <vipinverma4224@gmail.com>
2026-09-26 15:27:16 +0530

    changes done

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero';
 import TrustedBy from './components/TrustedBy';
 import Services from './components/Services';
-
+import OurWork from './components/OurWork';
+import Teams from './components/Teams';
 import { useState } from 'react';
+import ContactUs from './components/ContactUs';
 const App = () => {
   const[theme, setTheme] = useState(localStorage.getItem('theme') ? localStorage.getItem('theme'): 'light')
   return (
@@ -12,6 +14,9 @@ const App = () => {
       <Hero />
       <TrustedBy />
       <Services />
+      <OurWork/>
+      <Teams/>
+      <ContactUs/>
     </div>
   );
 };
